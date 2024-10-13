56fb7bfdbaca8b60448ba9704240a267a75e6bc3
Sebastian Schlenkrich <sebastian.schlenkrich@frame-consult.de>
2024-10-13 08:35:39 +0200

    Re-factor FX RA coupon pricer to incorporate smile

--- ql/cashflows/fxrangeaccrualfixed.cpp
+++ ql/cashflows/fxrangeaccrualfixed.cpp
@@ -1,4 +1,4 @@
-/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+﻿/* -*- mode: c++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
 /*
  Copyright (C) 2006, 2007 Giorgio Facchinetti
@@ -165,38 +165,81 @@ namespace QuantLib {
         additionalResults_.clear();
         // implement rangeAccrual_ calculation...
         Real minStd = 0.0005;  // 1% * sqrt(1d)
+        Real relSkewShift = 0.0001;
+        Real strikeLow = coupon.lowerTrigger();
+        Real strikeUpp = coupon.upperTrigger();
         CumulativeNormalDistribution Phi;
         Real daysInRange = 0.0;
         for (auto d : coupon.observationsSchedule()->dates()) {
             std::ostringstream s;
             s << io::iso_date(d);
             std::string date_s = s.str();
+            // we declare the valiables here to have them available for additional results later
             Real indexObservation = coupon.fxIndex()->fixing(d);
+            Real t = fxVolatility_->timeFromReference(d);
+            //
+            Real σLow0 = 0.0;
+            Real σLow1 = 0.0;
+            Real σUpp0 = 0.0;
+            Real σUpp1 = 0.0;
+            Real skewLow = 0.0;
+            Real skewUpp = 0.0;
             Real standardDevLow = 0.0;
             Real standardDevUpp = 0.0;
+            //
+            Real d1Low = 0.0;
+            Real d2Low = 0.0;
+            Real d1Upp = 0.0;
+            Real d2Upp = 0.0;
+            Real vegaLow = 0.0;
+            Real vegaUpp = 0.0;
+            Real putLow = 0.0;
+            Real putUpp = 0.0;
             if (d > fxVolatility_->referenceDate()) {
-                standardDevLow = std::sqrt(std::max(fxVolatility_->blackVariance(d, coupon.lowerTrigger(), true), 0.0));
-                standardDevUpp = std::sqrt(std::max(fxVolatility_->blackVariance(d, coupon.upperTrigger(), true), 0.0));
+                // low-strike calculations
+                σLow0 = std::max(fxVolatility_->blackVol(d, strikeLow, true), 0.0);
+                σLow1 = std::max(fxVolatility_->blackVol(d, (1.0 - relSkewShift) * strikeLow, true), 0.0);
+                skewLow = (σLow0 - σLow1) / (relSkewShift * strikeLow);
+                standardDevLow = σLow0 * std::sqrt(t);
+                // upp-strike calculations
+                σUpp0 = std::max(fxVolatility_->blackVol(d, strikeUpp, true), 0.0);
+                σUpp1 = std::max(fxVolatility_->blackVol(d, (1.0 + relSkewShift) * strikeUpp, true), 0.0);
+                skewUpp = (σUpp1 - σUpp0) / (relSkewShift * strikeUpp);
+                standardDevUpp = σUpp0 * std::sqrt(t);
             }
             Real inRangeProbability = 0.0;
-            if (standardDevLow < minStd) {  // calculate intrinsic value
-                if (indexObservation >= coupon.lowerTrigger() &&
-                    indexObservation <= coupon.upperTrigger())
-                    inRangeProbability = 1.0;
-            } else {  // put option spread valuation
-                Real d2Lower = std::log(indexObservation / coupon.lowerTrigger()) / standardDevLow -
-                               0.5 * standardDevLow;
-                Real d2Upper = std::log(indexObservation / coupon.upperTrigger()) / standardDevUpp -
-                               0.5 * standardDevUpp;
-                Real putLower = Phi(-d2Lower);
-                Real putUpper = Phi(-d2Upper);
-                inRangeProbability = putUpper - putLower;
+            //
+            if (standardDevLow < minStd) { // calculate intrinsic value
+                putLow = (indexObservation < strikeLow) ? (1.0) : (0.0);
+            } else { // Black digital put with smile
+                d1Low = std::log(indexObservation / strikeLow) / standardDevLow + 0.5 * standardDevLow;
+                d2Low = d1Low - standardDevLow;
+                putLow = Phi(-d2Low);
+                // smile ajustment
+                vegaLow = indexObservation * Phi.derivative(d1Low) * std::sqrt(t);
+                putLow += vegaLow * skewLow;
             }
+            //
+            if (standardDevUpp < minStd) { // calculate intrinsic value
+                putUpp = (indexObservation < strikeUpp) ? (1.0) : (0.0);
+            } else { // Black digital put with smile
+                d1Upp = std::log(indexObservation / strikeUpp) / standardDevUpp + 0.5 * standardDevUpp;
+                d2Upp = d1Upp - standardDevUpp;
+                putUpp = Phi(-d2Upp);
+                // smile ajustment
+                vegaUpp = indexObservation * Phi.derivative(d1Upp) * std::sqrt(t);
+                putUpp += vegaUpp * skewUpp;
+            }
+            inRangeProbability = putUpp - putLow;
             daysInRange += inRangeProbability;
             //
             additionalResults_["indexObservation_" + date_s] = indexObservation;
             additionalResults_["standardDevLow_" + date_s] = standardDevLow;
             additionalResults_["standardDevUpp_" + date_s] = standardDevUpp;
+            additionalResults_["vegaLow_" + date_s] = vegaLow;
+            additionalResults_["vegaUpp_" + date_s] = vegaUpp;
+            additionalResults_["skewLow_" + date_s] = skewLow;
+            additionalResults_["skewUpp" + date_s] = skewUpp;
             additionalResults_["inRangeProbability_" + date_s] = inRangeProbability;
         }
         rangeAccrual_ = daysInRange / coupon.observationsSchedule()->dates().size();
